54bbd6ccec1df7bf78096c3c3886a7664555c5a6
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-11-06 02:40:50 +0300

    add addEventListeners to GameConstroller.js

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -11,12 +11,19 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(themes.prairie);
+    this.addEventListeners();
     const teamGood = new Team('good');
     teamGood.add(1, 3);
     const teamEvil = new Team('evil');
     teamEvil.add(1, 3);
     const allPositionedCharacters = [...teamGood.positionedMembers, ...teamEvil.positionedMembers];
     this.gamePlay.redrawPositions(allPositionedCharacters);
+  }
+
+  addEventListeners() {
+    this.gamePlay.addCellEnterListener(this.onCellEnter);
+    this.gamePlay.addCellLeaveListener(this.onCellLeave);
+    this.gamePlay.addCellClickListener(this.onCellClick);
   }
 
   onCellClick(index) {
